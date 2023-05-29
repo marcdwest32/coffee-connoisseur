6187eb95dd3c568ec171cfd506c75fbf32d95594
Your marcdwest32 <marcdwest@gmail.com>
2023-05-29 18:44:32 -0500

    next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['images.unsplash.com'],
+    domains: [
+      'images.unsplash.com',
+      's3-media1.fl.yelpcdn.com',
+      's3-media2.fl.yelpcdn.com',
+      's3-media3.fl.yelpcdn.com',
+      's3-media4.fl.yelpcdn.com',
+    ],
   },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+    }
+
+    return config
+  },
+  env: {},
 }
 
 module.exports = nextConfig
