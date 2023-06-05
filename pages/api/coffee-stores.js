d49c5aae58321807c3cd39c89a6d22f7ea98a551
Your marcdwest32 <marcdwest@gmail.com>
2023-06-05 15:08:54 -0500

    fetchCoffeeStores

---
+++ pages/api/coffee-stores.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const axios = require('axios')
+const cors = require('cors')
+
+const app = express()
+const port = 3001 // Choose a port number for your server
+
+app.use(cors({ origin: 'http://localhost:3000' }))
+
+app.get('/coffee-stores', async (req, res) => {
+  const { latitude, longitude, limit } = req.query
+  const apiKey = process.env.YELP_API_KEY
+  try {
+    const response = await axios.get(
+      'https://api.yelp.com/v3/businesses/search',
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+        params: {
+          latitude,
+          longitude,
+          term: 'coffee',
+          radius: 10000,
+          sort_by: 'best_match',
+          limit,
+        },
+      },
+    )
+
+    return res.json(response.data.businesses)
+  } catch (error) {
+    console.error('API Error fetching coffee stores:', error)
+    res.status(500).json({ error: 'Failed to fetch coffee stores' })
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,18 +1,25 @@
-import { useRouter } from 'next/router'
-import { fetchCoffeeStores } from '../../lib/coffee_stores'
-import styles from '../../styles/coffee-store.module.css'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useContext, useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { StoreContext } from '../../store/store-context'
+import { fetchCoffeeStores } from '../../lib/coffee-stores'
+import { isEmpty } from '../../utils'
+import styles from '../../styles/coffee-store.module.css'
 import coffeeAlt from '../../public/images/coffee-alt.jpg'
 
-export async function getStaticProps({ params }) {
+export async function getStaticProps(staticProps) {
+  const params = staticProps.params
+  console.log('line 14 params', params)
+
   const coffeeStores = await fetchCoffeeStores()
+  const findCoffeeStoreById = coffeeStores.find((coffeeStore) => {
+    return coffeeStore.id.toString() === params.id //dynamic id
+  })
   return {
     props: {
-      coffeeStore: coffeeStores.find((coffeeStore) => {
-        return coffeeStore.id.toString() === params.id //dynamic id
-      }),
+      coffeeStore: findCoffeeStoreById ? findCoffeeStoreById : {},
     },
   }
 }
@@ -32,16 +39,58 @@ export async function getStaticPaths() {
   }
 }
 
-const CoffeeStore = (props) => {
-  const { id: storeId, name, image_url, url, location } = props.coffeeStore
-  const { address1: address, city, zip_code, state } = location
-  const neighborhood = `${city}, ${state} ${zip_code}`
-  const imgUrl = image_url || coffeeAlt
-
+const CoffeeStore = (initialProps) => {
   const router = useRouter()
   if (router.isFallback) {
     return <div>...Loading</div>
   }
+
+  const id = router.query.id
+
+  const {
+    state: { coffeeStores },
+  } = useContext(StoreContext)
+
+  const empty = isEmpty(initialProps.coffeeStore)
+
+  const [coffeeStore, setCoffeeStore] = useState(() => {
+    if (empty && coffeeStores.length > 0) {
+      return coffeeStores.find(
+        (coffeeStore) => coffeeStore.id.toString() === id,
+      )
+    }
+    return initialProps.coffeeStore
+  })
+
+  useEffect(() => {
+    if (empty && coffeeStores.length > 0) {
+      const findCoffeeStoreById = coffeeStores.find(
+        (coffeeStore) => coffeeStore.id.toString() === id,
+      )
+      console.log('line 70 coffeeStore', findCoffeeStoreById)
+      setCoffeeStore(findCoffeeStoreById)
+    }
+  }, [id, empty, coffeeStores])
+
+  //   console.log('line 60 empty', empty)
+  //   useEffect(() => {
+  //     console.log('line 59 coffeeStores', coffeeStores)
+  //     if (empty) {
+  //       if (coffeeStores.length > 0) {
+  //         const findCoffeeStoreById = coffeeStores.find((coffeeStore) => {
+  //           return coffeeStore.id.toString() === id //dynamic id
+  //         })
+  //         console.log('line 73 coffeeStore', findCoffeeStoreById)
+  //         setCoffeeStore(findCoffeeStoreById)
+  //       }
+  //     }
+  //   }, [id])
+
+  const { id: storeId, name, image_url, url, location = {} } = coffeeStore
+  const { address1: address, city, zip_code, state } = location
+  const neighborhood =
+    city && state && zip_code ? `${city}, ${state} ${zip_code}` : ''
+  const imgUrl = image_url || coffeeAlt
 
   const handleUpvoteButton = () => {
     console.log('upvote')
