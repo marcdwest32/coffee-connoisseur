6c308ab923831c0229fb9839ffbfc3d6640cb4e2
Your marcdwest32 <marcdwest@gmail.com>
2023-06-05 15:08:05 -0500

    hooks

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
+import { ACTION_TYPES, StoreContext } from '../store/store-context'
 import styles from '../styles/Home.module.css'
 
 import Banner from '../components/banner'
 import Card from '../components/card'
 import heroImage from '../public/images/hero-image.png'
-import { fetchCoffeeStores } from '../lib/coffee_stores'
+import { fetchCoffeeStores } from '../lib/coffee-stores'
 import useTrackLocation from '../hooks/useTrackLocation'
 
 export async function getStaticProps(context) {
   const coffeeStores = await fetchCoffeeStores()
+  console.log(coffeeStores)
 
   return {
     props: {
@@ -20,40 +22,24 @@ export async function getStaticProps(context) {
 }
 
 export default function Home(props) {
-  const { handleTrackLocation, isFindingLocation, latLong, locationErrorMsg } =
+  const { handleTrackLocation, isFindingLocation, locationErrorMsg } =
     useTrackLocation()
-  const [coffeeStores, setCoffeeStores] = useState('')
   const [coffeeStoresError, setCoffeeStoresError] = useState(null)
+  const { dispatch, state } = useContext(StoreContext)
+  const { coffeeStores, latLong } = state
 
-  //   console.log({ latLong, locationErrorMsg })
-
-  //   useEffect(() => {
-  //     async function setCoffeeStoresByLocation() {
-  //       if (latLong) {
-  //         console.log(latLong)
-  //         try {
-  //           const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 6)
-  //           console.log({ fetchedCoffeeStores })
-  //           //set coffee stores
-  //         } catch (error) {
-  //           //set error
-  //           console.log('Error', { error })
-  //         }
-  //       }
-  //     }
-  //     setCoffeeStoresByLocation()
-  //   }, [() => {}, latLong])
   useEffect(() => {
-    async function setCoffeeStoresByLocation() {
+    const setCoffeeStoresByLocation = async () => {
       if (latLong) {
         try {
-          const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 3)
+          const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 2)
           console.log({ fetchedCoffeeStores })
-          setCoffeeStores(fetchedCoffeeStores)
-          //set coffee stores
+          dispatch({
+            type: ACTION_TYPES.SET_COFFEE_STORES,
+            payload: { coffeeStores: fetchedCoffeeStores },
+          })
         } catch (error) {
-          //set error
-          console.log({ error })
+          console.error({ error })
           setCoffeeStoresError(error.message)
         }
       }
@@ -62,10 +48,8 @@ export default function Home(props) {
   }, [latLong])
 
   const handleOnBannerBtnClick = () => {
-    console.log('click')
     handleTrackLocation()
   }
-  //   console.log(props)
 
   return (
     <div className={styles.container}>
@@ -81,13 +65,35 @@ export default function Home(props) {
           handleOnClick={handleOnBannerBtnClick}
         ></Banner>
         {locationErrorMsg && <p>Something went wrong: {locationErrorMsg}</p>}
+        {coffeeStoresError && <p>Something went wrong: {coffeeStoresError}</p>}
         <div className={styles.heroImage}>
           <Image src={heroImage} width={700} height={400} alt='hero image' />
         </div>
+        {coffeeStores.length > 0 && (
+          <>
+            <div className={styles.sectionWrapper}>
+              <h2 className={styles.heading2}>Stores Near Me</h2>
+              <div className={styles.cardLayout}>
+                {coffeeStores.map((coffeeStore) => {
+                  return (
+                    <Card
+                      key={coffeeStore.id}
+                      //   id={coffeeStore.id}
+                      name={coffeeStore.name}
+                      imgUrl={coffeeStore.image_url}
+                      src={''}
+                      href={`coffee-store/${coffeeStore.id}`}
+                    />
+                  )
+                })}
+              </div>
+            </div>
+          </>
+        )}
         {props.coffeeStores.length > 0 && (
           <>
             <div className={styles.sectionWrapper}>
-              <h2 className={styles.heading2}>New Orleans Stores</h2>
+              <h2 className={styles.heading2}>Toronto Stores</h2>
               <div className={styles.cardLayout}>
                 {props.coffeeStores.map((coffeeStore) => {
                   return (
